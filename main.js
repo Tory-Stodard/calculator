a67629c84da951f512b39c32c286f4c6d89182f1
Tory Stodard <tory.stodard@gmail.com>
2023-08-15 20:57:53 -0600

    added operator function and related variables.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +16,17 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
 }
